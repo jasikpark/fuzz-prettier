60f2c8a09a87a880a373143fa811a383619c5a7d
Caleb Jasik (Personal) <calebjasik-personal@Calebs-MacBook-Pro.local>
2023-10-10 09:53:55 -0500

    add roundtrips

--- prettier-plugin-toml.fuzz.ts
+++ prettier-plugin-toml.fuzz.ts
@@ -1,4 +1,5 @@
 import "@jazzer.js/jest-runner";
+import assert from "node:assert";
 import prettier from "prettier";
 import * as prettierPluginTOML from "prettier-plugin-toml";
 
@@ -24,5 +25,37 @@ describe("prettier-plugin-toml", () => {
       }
       throw error;
     }
+  });
+
+  it.fuzz("prints a roundtrip correctly", async (data: Buffer) => {
+    let formatted: string;
+
+    try {
+      formatted = await prettier.format(data.toString(), {
+        parser: "toml",
+        plugins: [prettierPluginTOML],
+      });
+    } catch (error: unknown) {
+      // check that the error message returns this value
+      if (
+        error instanceof SyntaxError ||
+        (error instanceof Error && error?.message.includes("SyntaxError")) ||
+        (error instanceof Error &&
+          error?.message.includes("Sad sad panda, lexing errors")) ||
+        (error instanceof Error &&
+          error?.message.includes("Sad sad panda, parsing errors detected")) ||
+        (typeof error === "string" && error.includes("SyntaxError"))
+      ) {
+        return;
+      }
+      throw error;
+    }
+
+    const formatted_twice = await prettier.format(data.toString(), {
+      parser: "toml",
+      plugins: [prettierPluginTOML],
+    });
+
+    assert.deepStrictEqual(formatted, formatted_twice);
   });
 });

ff6a4de39c04eebfb4246a7b6bc7a8062f23226e
Caleb Jasik (Personal) <calebjasik-personal@Calebs-MacBook-Pro.local>
2023-10-11 20:45:20 -0500

    upgrade astro to the new setup

---
+++ src/prettier-plugin-astro.fuzz.ts
@@ -0,0 +1,41 @@
+import "@jazzer.js/jest-runner";
+import prettier from "prettier";
+import * as prettierPluginAstro from "prettier-plugin-astro";
+import { idempotent } from "./util/idempotent.js";
+import { naive } from "./util/naive.js";
+
+describe("prettier-plugin-astro", () => {
+  it.fuzz(
+    "formats a basic setup",
+    naive(
+      async (input: string) =>
+        await prettier.format(input, {
+          parser: "astro",
+          plugins: [prettierPluginAstro],
+        }),
+      (error) => {
+        if (false) {
+          return;
+        }
+        throw error;
+      }
+    )
+  );
+
+  it.fuzz(
+    "prints a roundtrip correctly",
+    idempotent(
+      async (input: string) =>
+        await prettier.format(input, {
+          parser: "astro",
+          plugins: [prettierPluginAstro],
+        }),
+      (error) => {
+        if (false) {
+          return;
+        }
+        throw error;
+      }
+    )
+  );
+});

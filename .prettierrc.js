b10633a5dfe8ceff0042efb7ac701b323fa62d75
Caleb Jasik <calebjasik@jasik.xyz>
2023-11-28 16:47:45 -0600

    `prettier-plugin-autocorrect`

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: ["prettier-plugin-pkg"],
+  plugins: ["prettier-plugin-pkg", "prettier-plugin-autocorrect"],
 };
 
 export default config;
